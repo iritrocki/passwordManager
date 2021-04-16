5b969d511140cb617be7a808b78a1fe78d3d08f6
mlijtenstein <mlijtenstein@gmail.com>
2021-04-16 18:29:50 -0300

    Actualizo metodos al ingles

---
+++ gestorDeContrasenas/gestor/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace passwordManager
+{
+    public class Category
+    {
+        public Category(){}
+
+        public Category(string v)
+        {
+            name = v;
+        }
+
+        public string name { get; set; }
+
+        public void update(Category c, string v)
+        {
+            c.name = v;
+        }
+
+        public bool validateName(string unString)
+        {
+            if(unString.Length >= 3 && unString.Length <= 15)
+                return true;
+            return false;
+        }
+
+    }
+}
