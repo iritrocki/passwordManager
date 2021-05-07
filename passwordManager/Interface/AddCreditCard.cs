ca4d2cf390f426e65caab0e46beb10cc39392849
matiaslijtenstein <matiaslij@gmail.com>
2021-05-07 18:31:59 -0300

    Termine ventanas de CreditCard

---
+++ passwordManager/Interface/AddCreditCard.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using passwordManager;
+using passwordManager.Exceptions;
+
+namespace Interface
+{
+    public partial class AddCreditCard : UserControl
+    {
+        private const int MONTH = 0;
+        private const int YEAR = 1;
+        private User user;
+        private CreditCard modificationCreditCard;
+        public AddCreditCard(User u)
+        {
+            lblCreditCardError.Text = "";
+            this.user = u;
+            InitializeComponent();
+            ChargeComboBox();
+        }
+        public AddCreditCard(User u, CreditCard creditCard)
+        {
+
+            this.user = u;
+            this.modificationCreditCard = creditCard;
+            InitializeComponent();
+            ChargeComboBox();
+        }
+
+        public void ChargeComboBox()
+        {
+
+            comboBoxCreditCardCategory.DataSource = user.Categories;
+            comboBoxCreditCardCategory.DisplayMember = "Name";
+            /*comboBoxCreditCardCategory.Items.Add(string.Format("{0}",category.Name));*/
+
+        }
+
+
+        public int GetExpirationDate(string date, int index)
+        {
+            string[] expirationDate = date.Split('/');
+            int integer = Int32.Parse(expirationDate[index]);
+            return integer;
+        }
+
+        private void btnAcceptNewCreditCard_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                CreditCard newCreditCard = CreateNewCreditCard();
+
+                if (modificationCreditCard == null)
+                {
+                    user.TryAddCreditCard(newCreditCard);
+                }
+                else
+                {
+                    user.TryModifyCreditCard(modificationCreditCard, newCreditCard);
+                }
+            }
+            catch (InvalidCreditCardException exc)
+            {
+                lblCreditCardError.Text = exc.Message;
+            }
+        }
+
+        private CreditCard CreateNewCreditCard()
+        {
+            Category category = comboBoxCreditCardCategory.SelectedItem as Category;
+            string name = txtCreditCardName.Text;
+            string company = txtCreditCardCompany.Text;
+            string number = txtCreditCardNumber.Text;
+            string code = txtCreditCardCode.Text;
+            int expirationMonth = GetExpirationDate(txtCreditCardExpiration.Text, MONTH);
+            int expirationYear = GetExpirationDate(txtCreditCardExpiration.Text, YEAR);
+            string notes = txtCreditCardNotes.Text;
+
+            CreditCard newCreditCard = new CreditCard()
+            {
+                Name = name,
+                Company = company,
+                Number = number,
+                Code = code,
+                ExpirationMonth = expirationMonth,
+                ExpirationYear = expirationYear,
+                Category = category
+            };
+            return newCreditCard;
+        }
+    }
+}
---
+++ passwordManager/Interface/CreditCardList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using passwordManager;
+
+namespace Interface
+{
+    public partial class CreditCardList : UserControl
+    {
+        private User user;
+        private Panel MainPanel;
+        public CreditCardList(User u,Panel p)
+        {
+            lblErrorCreditCard.Text = "";
+            InitializeComponent();
+            this.user = u;
+            this.MainPanel = p;
+            ChargeCreditCardsToList();
+        }
+
+        public void ChargeCreditCardsToList()
+        {
+            listViewCreditCards.Items.Clear();
+            foreach (CreditCard cc in user.CreditCards)
+            {
+                string[] row = new string[] { cc.Category.Name, cc.Name, cc.Company, cc.Number, string.Format("{0}/{1}",cc.ExpirationMonth,cc.ExpirationYear)};
+                ListViewItem item = new ListViewItem(row);
+                item.Tag = cc;
+                listViewCreditCards.Items.Add(item);
+            }
+        }
+
+        private void btnAddCreditCard_Click(object sender, EventArgs e)
+        {
+            UserControl creditCardEditWindow = new AddCreditCard(user);
+            this.MainPanel.Controls.Clear();
+            this.MainPanel.Controls.Add(creditCardEditWindow);
+        }
+
+        private void btnModifyCreditCard_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CreditCard selectedCreditCard = (CreditCard)listViewCreditCards.SelectedItems[0].Tag;
+                UserControl creditCardEditWindow = new AddCreditCard(user,selectedCreditCard);
+                this.MainPanel.Controls.Clear();
+                this.MainPanel.Controls.Add(creditCardEditWindow);
+            }
+            catch (Exception exc)
+            {
+                lblErrorCreditCard.Text = "Debe seleccionar una tarjeta para modificarla";
+            }
+        }
+
+        private void btnDeleteCreditCard_Click(object sender, EventArgs e)
+        {
+            try{
+                CreditCard selectedCreditCard = (CreditCard)listViewCreditCards.SelectedItems[0].Tag;
+                user.TryRemoveCreditCard(selectedCreditCard);
+            }
+            catch (Exception exc)
+            {
+                lblErrorCreditCard.Text = "Debe seleccionar una tarjeta para eliminarla";
+            }
+        }
+    }
+}
