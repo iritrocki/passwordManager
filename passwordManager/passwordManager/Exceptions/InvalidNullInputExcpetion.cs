3d74c3d8eebca202d048f78d609dd7abbfbeae4e
matiaslijtenstein <matiaslij@gmail.com>
2021-05-10 17:19:42 -0300

    Agregue validacion para input null de categoria

---
+++ passwordManager/passwordManager/Exceptions/InvalidNullInputExcpetion.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace passwordManager.Exceptions
+{
+    [Serializable]
+    public class InvalidNullInputExcpetion : Exception
+    {
+        private string message;
+
+        public override string Message
+        {
+            get { return message; }
+        }
+
+        public InvalidNullInputExcpetion()
+        {
+            this.message = "La categoria no puede ser nula";
+        }
+    }
+}
